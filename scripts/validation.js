dc61e9cd49c7a5369850b6ea72b9aa1664cf98a4
Butterkeks <christiane.sarau@gmail.com>
2016-04-03 22:49:00 +0200

    final version html5 validierun

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,16 +1,62 @@
 
 $(function () {
+    var $fname = $("#fname");
+    var $lname = $("#lname");
+    var $bday = $("#bday");
+    var $password = $("#password");
+    var $email = $("#email");
+    var $AGB = $("#AGB");
+    var fields = [$fname, $lname, $bday, $password, $email];
 
-   // if(!hasFormValidation()) {
-        var $fname = $("#fname");
-        var $lname = $("#lname");
-        var $bday = $("#bday");
-        var $password = $("#password");
-        var $email = $("#email");
-        var $AGB = $("#AGB");
-        var fields = [$fname, $lname, $bday, $password, $email];
 
+    function isMinAge(normalizedBirthday) {
+        var minAge = 18;
+        var tempDate = new Date(normalizedBirthday.getFullYear() + minAge, normalizedBirthday.getMonth(), normalizedBirthday.getDate());
+        return (tempDate <= new Date());
+    }
+    function isValidDate(normalizedBirthday) {
+        return !isNaN(normalizedBirthday.getTime());
+    }
+
+    if(!hasNativeDateInput() || !hasFormValidation()) {
+        var validateBirthday = function () {
+            var normalizedDate = getNormalizedDateString("#bday").split(".");
+            var normalizedBirthday = new Date(normalizedDate[2], normalizedDate[1] - 1, normalizedDate[0]);
+            var elem = $(this);
+
+            elem.find("~ .error-invalid-date").hide();
+            elem.find("~ .error-not-born-yet").hide();
+            elem.find("~ .error-too-young").hide();
+            elem.data('valid', true);
+
+            if (!isValidDate(normalizedBirthday)) {
+                elem.find("~ .error-invalid-date").show();
+                elem.data('valid', false);
+            } else if (normalizedBirthday > new Date()) {
+                elem.find("~ .error-not-born-yet").show();
+                elem.data('valid', false);
+            } else if (!isMinAge(normalizedBirthday)) {
+                elem.find("~ .error-too-young").show();
+                elem.data('valid', false);
+            }
+
+            if (hasFormValidation()) {
+                if (elem.data('valid')) {
+                    elem.setCustomValidity();
+                } else {
+                    elem.setCustomValidity("Sie müssen mindestens 18 Jahre alt sein.");
+                }
+            } else {
+                checkValidForm();
+            }
+        };
+
+        $bday.on('focus keyup change', validateBirthday);
+    }
+
+    if(!hasFormValidation()) {
         var $submitButton = $("#submitButton");
+        $submitButton.attr('disabled', true);
 
         for (var field in fields) {
             fields[field].data('valid', false);
@@ -27,7 +73,6 @@ $(function () {
             $submitButton.removeAttr('disabled');
         }
 
-        /*TODO:HTML5 validation*/
         var validateMandatory = function () {
             var elem = $(this);
             var val = elem.val();
@@ -43,51 +88,12 @@ $(function () {
             checkValidForm();
         };
 
-        $fname.on('focus keyup', validateMandatory);
-        $lname.on('focus keyup', validateMandatory);
-        $bday.on('focus keyup', validateMandatory);
-        $password.on('focus keyup', validateMandatory);
-        $email.on('focus keyup', validateMandatory);
+        $fname.on('focus keyup change', validateMandatory);
+        $lname.on('focus keyup change', validateMandatory);
+        $bday.on('focus keyup change', validateMandatory);
+        $password.on('focus keyup change', validateMandatory);
+        $email.on('focus keyup change', validateMandatory);
 
-        function isMinAge(normalizedBirthday) {
-            var minAge = 18;
-            var tempDate = new Date(normalizedBirthday.getFullYear() + minAge, normalizedBirthday.getMonth(), normalizedBirthday.getDate());
-            return (tempDate <= new Date());
-        }
-
-        function isValidDate(normalizedBirthday) {
-            return !isNaN(normalizedBirthday.getTime());
-        }
-
-        if(!hasNativeDateInput()) {
-            var validateBirthday = function () {
-                var normalizedDate = getNormalizedDateString("#bday").split(".");
-                var normalizedBirthday = new Date(normalizedDate[2], normalizedDate[1] - 1, normalizedDate[0]);
-                var elem = $(this);
-
-                elem.find("~ .error-invalid-date").hide();
-                elem.find("~ .error-not-born-yet").hide();
-                elem.find("~ .error-too-young").hide();
-                elem.data('valid', true);
-
-                if (!isValidDate(normalizedBirthday)) {
-                    elem.find("~ .error-invalid-date").show();
-                    elem.data('valid', false);
-                } else if (normalizedBirthday > new Date()) {
-                    elem.find("~ .error-not-born-yet").show();
-                    elem.data('valid', false);
-                } else if (!isMinAge(normalizedBirthday)) {
-                    elem.find("~ .error-too-young").show();
-                    elem.data('valid', false);
-                }
-
-                checkValidForm();
-            };
-        }
-
-        $bday.on('change', validateBirthday);
-
-        /*TODO:HTML5 validation*/
         var validateEmail = function () {
             var elem = $(this);
             var regex = /^\S+@\S+\.\S+$/;
@@ -103,9 +109,8 @@ $(function () {
             checkValidForm();
         };
 
-        $email.on('change', validateEmail);
+        $email.on('focus keyup change', validateEmail);
 
-        /*TODO:HTML5 validation*/
         var validatePassword = function () {
             var elem = $(this);
             var val = elem.val();
@@ -114,18 +119,20 @@ $(function () {
             elem.find("~ .error-too-long").hide();
             elem.data('valid', true);
 
-            if (val.length < 4) {
+            if (val.length < 4 && val.length != 0) {
                 elem.find("~ .error-too-short").show();
                 elem.data('valid', false);
             } else if (val.length > 8) {
                 elem.find("~ .error-too-long").show();
+                elem.data('valid', false);
+            } else if (val.length == 0) {
                 elem.data('valid', false);
             }
 
             checkValidForm();
         };
 
-        $password.on('change', validatePassword);
+        $password.on('focus keyup change', validatePassword);
 
         var validateAGB = function () {
             var val = $AGB.is(':checked');
@@ -139,8 +146,10 @@ $(function () {
         };
 
         $submitButton.on('click', function () {
-            validateAGB();
-            alert('Vielen Dank!');
-        })
-   // }
+            var validAGB = validateAGB();
+            if (validAGB) {
+                alert('Vielen Dank!');
+            }
+        });
+    }
 });
