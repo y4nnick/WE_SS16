f460f904576145d4fc9ddf5777049f720fa1d57f
Yannick <y.schwarenthorer@icloud.com>
2016-04-24 22:36:39 +0200

    remove bid price from input field

--- Uebung2/lab2/src/main/webapp/scripts/BiddingEventInit.js
+++ Uebung2/lab2/src/main/webapp/scripts/BiddingEventInit.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
                 $(".bid-error").hide();
                 $('.highest-bid').text(data.price + "€");
                 $('.highest-bidder').text(data.bidder);
+                $(".bid-form-field").val("");
 
                 $(".balance").html(data.balance + "€");
                 $(".running-auctions-count").html(data.running);
